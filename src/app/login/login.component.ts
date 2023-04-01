18d14d21c8ebe25fddba40b805ac854611ac389c
hajjimajed <hajjimajed78@gmail.com>
2023-04-01 15:14:26 +0100

    adding routers and login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  username = '';
+  password = '';
+  errorMessage = 'Invalid credentials !';
+  invalidLogin = false;
+
+  constructor(private router: Router) { }
+
+  handleLogin() {
+    if (this.username === "majed" && this.password === "1234") {
+      this.invalidLogin = false;
+      this.router.navigate([('welcome')])
+    }
+    else {
+      this.invalidLogin = true;
+    }
+  }
+
+}
