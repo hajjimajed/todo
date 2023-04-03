55a93a0d748b0f5bd4534b028daa7fc4ace5dafe
hajjimajed <hajjimajed78@gmail.com>
2023-04-03 14:55:34 +0100

    finishing menu and footer components

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +19,8 @@ import { Component } from '@angular/core';
 export class ListTodosComponent {
 
   todos = [
-    { id: 1, description: 'learn to code' },
-    { id: 2, description: 'learn to dance' },
-    { id: 3, description: 'learn to drive' },
+    new Todo(1, 'learn to code', false, new Date()),
+    new Todo(2, 'be an expert', false, new Date()),
   ]
 
 }
