c31aa5557c2242eae9d5163c3c66b22f19fcb8cc
hajjimajed <hajjimajed78@gmail.com>
2023-04-06 14:03:49 +0100

    linking app with spring boot and creating data service

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -10,12 +11,20 @@ export class WelcomeComponent implements OnInit {
 
   name = ''
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private welcomeService: WelcomeDataService
+  ) { }
 
   ngOnInit() {
 
     this.name = this.route.snapshot.params['name'];
 
+  }
+
+  getWelcomeMessage() {
+    console.log(this.welcomeService.ExecuteHelloWorld())
+    this.welcomeService.ExecuteHelloWorld().subscribe()
   }
 
 }
